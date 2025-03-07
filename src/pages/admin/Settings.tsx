1a5dd33fdd194358d6f33452cc55cd571b13797b
Aayush Adhikari <adhikariaayush37@gmail.com>
2025-03-07 23:36:40 +0545

    Update: solve minor issues in settings page

--- src/pages/admin/Settings.tsx
+++ src/pages/admin/Settings.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useStore } from '@/lib/store';
 import { Save, Settings as SettingsIcon, Clock, Bell, Lock, Globe } from 'lucide-react';
 import { cn } from '@/lib/utils';
